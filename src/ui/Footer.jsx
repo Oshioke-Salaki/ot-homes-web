69fa44a0d0e8784e0810d089c335dc42e6907331
Oshioke-Salaki <salaki1902@gmail.com>
2023-11-17 14:57:47 -0600

    update

---
+++ src/ui/Footer.jsx
@@ -0,0 +1,22 @@
+import Logo from "./Logo";
+
+function Footer() {
+  return (
+    <div className="flex gap-x-[23px] pb-[67px] pl-[108px] pt-[83px] text-[#46465E]">
+      <div className="w-[405px]">
+        <Logo />
+        <p className="w-[365px] text-base font-normal ">
+          Our mission is to empower you with cutting-edge solutions that elevate
+          your performance, amplify your growth, and establish lasting
+          connections with your audience. Join us in shaping a brighter future
+          for your business.
+        </p>
+      </div>
+      <div>
+        <h1 className="text-[#000018]">Platform</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
