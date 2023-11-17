0ff8c25ce34263f8ab6a060659867edbc4edf0ad
Oshioke-Salaki <salaki1902@gmail.com>
2023-11-17 15:12:50 -0600

    update

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -13,29 +13,29 @@ function Footer() {
           for your business.
         </p>
       </div>
-      <div>
-        <div className="p-[10px]">
+      <div className="flex gap-x-[31px]">
+        <div className="flex flex-col gap-x-4 p-[10px]">
           <h1 className="font-semibold text-[#000018]">Platform</h1>
           <NavLink name="Analytics" />
           <NavLink name="Planning" />
           <NavLink name="Data managment" />
           <NavLink name="Security" />
         </div>
-        <div className="p-[10px]">
+        <div className="flex flex-col gap-x-4 p-[10px]">
           <h1 className="font-semibold text-[#000018]">Connect With Us</h1>
           <NavLink name="Instagram" />
           <NavLink name="Twitter" />
           <NavLink name="Facebook" />
           <NavLink name="LinkedIn" />
         </div>
-        <div className="p-[10px]">
+        <div className="flex flex-col gap-x-4 p-[10px]">
           <h1 className="font-semibold text-[#000018]">Reasources</h1>
           <NavLink name="Customers" />
           <NavLink name="Ebooks & Guides" />
           <NavLink name="Podacast & video" />
           <NavLink name="Finance" />
         </div>
-        <div className="p-[10px]">
+        <div className="flex flex-col gap-x-4 p-[10px]">
           <h1 className="font-semibold text-[#000018]">Our Services</h1>
           <NavLink name="About Us" />
           <NavLink name="Our Plans" />
