f086a3b7d34f14401c6e2e2c3a35a611217564e8
Oshioke-Salaki <salaki1902@gmail.com>
2023-11-17 13:48:40 -0600

    update

--- src/ui/ChooseCard.jsx
+++ src/ui/ChooseCard.jsx
@@ -4,7 +4,7 @@ function ChooseCard({ img, title, description }) {
   return (
     <div className="flex items-center gap-x-2 bg-white px-2 py-4">
       <div className="flex h-11 w-11 items-center justify-center rounded-full bg-[#E8F2FF] p-[10]">
-        <img src={img} alt="" />
+        <img src={img} alt="" className="h-6 w-6" />
       </div>
       <div className="flex flex-col gap-y-2">
         <h1 className="text-base font-semibold capitalize">{title}</h1>
